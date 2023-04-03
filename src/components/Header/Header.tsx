c47c90d2eaa33b8096935b44ca06382a611ac8bd
grishanova-oa <60963701+grishanova-oa@users.noreply.github.com>
2023-04-03 14:05:00 +0300

    Revert "React hooks"

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,15 @@
-import React, { useState } from 'react';
-import { NavLink, Outlet, useLocation } from 'react-router-dom';
+import React, { Component } from 'react';
+import { NavLink, Outlet } from 'react-router-dom';
 import { Footer } from '../Footer';
 import './HeaderStyles.css';
+import { withRouter } from './WithRouter';
+
+interface IHeaderState {
+  currentPage: string;
+}
+type HeaderProps = {
+  location: Location;
+};
 
 export const getCurrentPage = (pathname: string) => {
   let res = '';
@@ -25,36 +33,47 @@ export const getCurrentPage = (pathname: string) => {
   return res;
 };
 
-export const Header = () => {
-  const location = useLocation();
-  const [currentPage, setCurrentPage] = useState(getCurrentPage(location.pathname));
+export class Header extends Component<HeaderProps, IHeaderState> {
+  constructor(props: HeaderProps) {
+    super(props);
+    const { location } = this.props;
 
-  const handleNavClick = (newPage: string) => {
-    setCurrentPage(newPage);
+    this.state = {
+      currentPage: getCurrentPage(location.pathname),
+    };
+  }
+
+  handleNavClick = (currentPage: string) => {
+    this.setState({ currentPage });
   };
 
-  return (
-    <>
-      <header className="container__header">
-        <div className="header">
-          <div className="header_inner">
-            <NavLink to="/" className="header_logo" />
-            <div className="title">Lovely House</div>
-            <h1>{currentPage}</h1>
-            <NavLink to="/" onClick={() => handleNavClick('Main')} aria-label="cost-input">
-              Main
-            </NavLink>
-            <NavLink to="/form" onClick={() => handleNavClick('Form')}>
-              Form
-            </NavLink>
-            <NavLink to="/about" onClick={() => handleNavClick('About')}>
-              About us
-            </NavLink>
+  render() {
+    const { currentPage } = this.state;
+    return (
+      <>
+        <header className="container__header">
+          <div className="header">
+            <div className="header_inner">
+              <NavLink to="/" className="header_logo" />
+              <div className="title">Lovely House</div>
+              <h1>{currentPage}</h1>
+              <NavLink to="/" onClick={() => this.handleNavClick('Main')} aria-label="cost-input">
+                Main
+              </NavLink>
+              <NavLink to="/form" onClick={() => this.handleNavClick('Form')}>
+                Form
+              </NavLink>
+              <NavLink to="/about" onClick={() => this.handleNavClick('About')}>
+                About us
+              </NavLink>
+            </div>
           </div>
-        </div>
-      </header>
-      <Outlet />
-      <Footer />
-    </>
-  );
-};
+        </header>
+        <Outlet />
+        <Footer />
+      </>
+    );
+  }
+}
+
+export const HeaderComp = withRouter(Header);
