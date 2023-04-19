c3aee2bae2e577acf3800b520337230945233ff1
grishanova-oa <grishanova.oa@gmail.com>
2023-04-19 19:14:41 +0200

    fix: fix Warning in test InputTitle

--- src/components/InputTitle/InputTitle.tsx
+++ src/components/InputTitle/InputTitle.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 interface IInputTitle {
   title: string;
-  // inputTitleRef: React.RefObject<HTMLInputElement>
+  inputTitleRef: React.RefObject<HTMLInputElement>
 }
 
-export const InputTitle = ({ title }: IInputTitle) => (
+export const InputTitle = ({ title, inputTitleRef }: IInputTitle) => (
   <div className="form">
     <label htmlFor="text">Enter the City:</label>
-    <input type="text" name="inputTitle" className="inputTitle" aria-label="cost-input" />
+    <input type="text" name="inputTitle" className="inputTitle" aria-label="cost-input" ref={inputTitleRef} />
     {title && <div className="red">{title}</div>}
   </div>
 );
